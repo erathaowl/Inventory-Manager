7200471ace9f2c8e57ab51e9cda5f32acd36b1f6
erathaowl <f.mazzocchi@olympos.lan>
2023-04-02 08:07:46 +0200

    Fixed application launch and added basic (debug/bind host/bind port) command line arguments

--- flaskinventory/__init__.py
+++ flaskinventory/__init__.py
@@ -2,14 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'PleaseChangeMe'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 
-from flaskinventory import routes
+with app.app_context():
+    from flaskinventory import routes
 
-db.create_all()
-db.session.commit()
+    db.create_all()
+    db.session.commit()
--- run.py
+++ run.py
@@ -1,5 +1,32 @@
 from flaskinventory import app
+from argparse import ArgumentParser 
+import os
 
-if __name__ == '__main__':
+
+ENV_PREFIX = "FLASKINV"
+
+
+def parse_arguments():
+    p = ArgumentParser()
+    p.add_argument("-d", "--debug", 
+                   action="store_true",
+                   dest="debug",
+                   default=os.getenv(f"{ENV_PREFIX}_DEBUG", "false").lower() == "true",
+                   help="Enable debug mode")
+    p.add_argument("--host", 
+                   action="store",
+                   dest="host",
+                   default=os.getenv(f"{ENV_PREFIX}_HOST", "127.0.0.1"),
+                   help="Select binding host")
+    p.add_argument("--port", 
+                   action="store",
+                   dest="port",
+                   default=int(os.getenv(f"{ENV_PREFIX}_PORT", "5000")),
+                   help="Select binfing port")
+    return p.parse_args()
     
-    app.run()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    app.run(host=args.host, port=args.port, debug=args.debug)
